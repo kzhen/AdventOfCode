f89b0226618877249cdb93b66e1f5fb711eb354a
Ken <dev@kenpire.com>
2021-12-07 22:58:02 +0000

    Added some solutions for 2020

---
+++ AdventOfCode/2020/Day25.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2020
+{
+    public class Day25 : PuzzleBase<long[]>
+    {
+        public Day25() : base(25, 2020, "14897079", "")
+        {
+
+        }
+
+        public override long[] ParseInput(IEnumerable<string> input)
+        {
+            return input.Select(long.Parse).ToArray();
+        }
+
+        public override string SolveProblem1(long[] input)
+        {
+            var publicKeyCard = input[0];
+            var publicKeyDoor = input[1];
+
+            var cardSubjectNumber = 7;
+            var doorSubjectNumber = 7;
+
+            int doorLoopSize = GetLoopSize(publicKeyDoor);
+            int cardLoopSize = GetLoopSize(publicKeyCard);
+
+
+
+            //long cardEncryptionKey = Transform(publicKeyCard, doorLoopSize);
+            long doorEncryptionKey = Transform(publicKeyDoor, cardLoopSize);
+
+            return doorEncryptionKey.ToString(); ;
+        }
+
+        private long Transform(long subjectNumber, int cardLoopSize)
+        {
+            long result = 1;
+            for (int i = 0; i < cardLoopSize; i++)
+            {
+                var tempResult = result * subjectNumber;
+                result = (tempResult % 20201227);
+            }
+
+            return result;
+        }
+
+        public int TransformUntil(long subjectNumber, long target)
+        {
+            long result = 1;
+            int loopSize = 0;
+            do
+            {
+                var tempResult = result * subjectNumber;
+                result = (tempResult % 20201227);
+
+                loopSize++;
+            } while (result != target);
+
+
+            return loopSize;
+        }
+
+        private int GetLoopSize(long publicKeyDoor)
+        {
+            var loopSize = TransformUntil(7, publicKeyDoor);
+
+            return loopSize;
+
+            for (int i = 1; i < 100000; i++)
+            {
+                var res = Transform(7, i);
+                if (res == publicKeyDoor)
+                {
+                    return i;
+                }
+            }
+            throw new InvalidOperationException("oops");
+        }
+
+        public override string SolveProblem2(long[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode/2020/Day8.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2020
+{
+    public record Instruction(string opCode, int val);
+    public class Day8 : PuzzleBase<Dictionary<int, Instruction>>
+    {
+        public Day8() : base(8, 2020, "5", "8")
+        {
+
+        }
+        public override Dictionary<int, Instruction> ParseInput(IEnumerable<string> input)
+        {
+            var res = input.Select((i, j) => new { Pos = j, Inst = new Instruction(i.Split(" ")[0], int.Parse(i.Split(" ")[1])) });
+            return res.ToDictionary(k => k.Pos, v => v.Inst);
+        }
+
+        public override string SolveProblem1(Dictionary<int, Instruction> input)
+        {
+            int pos = 0;
+            int accumulator = 0;
+            List<int> visitedLines = new List<int>();
+            do
+            {
+                if (visitedLines.Contains(pos))
+                {
+                    break;
+                }
+
+                var instruction = input[pos];
+                visitedLines.Add(pos);
+
+                switch (instruction.opCode)
+                {
+                    case "acc":
+                        accumulator += instruction.val;
+                        pos++;
+                        break;
+                    case "nop":
+                        pos++;
+                        break;
+                    case "jmp":
+                        pos += instruction.val;
+                        break;
+                    default:
+                        break;
+                }
+
+
+
+            } while (true);
+
+            //9177528 too high
+            return accumulator.ToString();
+        }
+
+        public override string SolveProblem2(Dictionary<int, Instruction> input)
+        {
+            var exitOnLine = input.Count;
+
+            foreach (var nopOrJump in input.Where(kvp => kvp.Value.opCode.Equals("nop") || kvp.Value.opCode.Equals("jmp")))
+            {
+                int pos = 0;
+                int accumulator = 0;
+                List<int> visitedLines = new List<int>();
+
+                do
+                {
+                    if (visitedLines.Contains(pos))
+                    {
+                        break;
+                    }
+
+                    var currentInstruction = input[pos];
+
+                    if (pos == nopOrJump.Key)
+                    {
+                        //switch
+                        var invertedOpCode = currentInstruction.opCode.Equals("jmp") ? "nop" : "jmp";
+                        currentInstruction = currentInstruction with { opCode = invertedOpCode };
+                    }
+
+                    visitedLines.Add(pos);
+
+                    switch (currentInstruction.opCode)
+                    {
+                        case "acc":
+                            accumulator += currentInstruction.val;
+                            pos++;
+                            break;
+                        case "nop":
+                            pos++;
+                            break;
+                        case "jmp":
+                            pos += currentInstruction.val;
+                            break;
+                        default:
+                            break;
+                    }
+
+
+                    if (pos == exitOnLine)
+                    {
+                        return accumulator.ToString();
+                    }
+
+                } while (true);
+
+            }
+
+            return "ooops".ToString();
+        }
+    }
+}
